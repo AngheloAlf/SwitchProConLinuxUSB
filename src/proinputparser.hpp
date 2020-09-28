785eb9df9bde063f54b9bd1436c248fe288a1160
angie <angheloalf95@gmail.com>
2020-09-28 12:31:20 -0300

    parser: correctly handle standard input report

--- src/proinputparser.hpp
+++ src/proinputparser.hpp
@@ -77,8 +77,8 @@ public:
   }
 
   bool is_button_pressed(BUTTONS button) const {
-    uint8_t pos = data_address(button);
-    return dat[pos] & byte_button_value(button);
+    uint8_t pos = buttons_data_address(button);
+    return dat[pos] & buttons_byte_button_value(button);
   }
 
   uint16_t get_axis_status(AXIS axis) const {
@@ -116,14 +116,14 @@ public:
       n_bad_data_thirty++;
     if (dat == 0x00)
       n_bad_data_zero++;*/
-    return (dat[0] == 0x30 || dat[0] == 0x00);
+    return (/*dat[0] == 0x30 ||*/ dat[0] == 0x00);
   }
 
   void print() const {
-    print_exchange_array(dat);
+    print_exchange_array(dat, type);
   }
 
-  static uint8_t bit_position(BUTTONS button) {
+  static uint8_t buttons_bit_position(BUTTONS button) {
     switch (button) {
     case Y:
     case minus:
@@ -154,7 +154,7 @@ public:
     }
   }
 
-  static uint8_t byte_button_value(BUTTONS button) {
+  static uint8_t buttons_byte_button_value(BUTTONS button) {
     switch (button) {
     case Y:
     case minus:
@@ -185,60 +185,116 @@ public:
     }
   }
 
-  static size_t data_address(BUTTONS button) {
+  static size_t buttons_data_address(BUTTONS button, PacketType packet) {
+    std::array<size_t, 3> address;
+    switch (packet) {
+    case PacketType::standard_input_report:
+      address[0x00] = 0x03;
+      address[0x01] = 0x04;
+      address[0x02] = 0x05;
+      break;
+    case PacketType::packet_req:
+      address[0x00] = 0x0d;
+      address[0x01] = 0x0e;
+      address[0x02] = 0x0f;
+      break;
+    default:
+      throw std::domain_error("Error: This packet type does not contain button data.");
+    }
     switch (button) {
-    case A:
-    case B:
-    case X:
-    case Y:
-    case R1:
-    case R2:
-      return 0x0d;
-    case plus:
-    case minus:
-    case home:
-    case share:
-    case L3:
-    case R3:
-      return 0x0e;
-    case L1:
-    case L2:
-      return 0x0f;
-    case None:
-      return 0x00;
+    case BUTTONS::A:
+    case BUTTONS::B:
+    case BUTTONS::X:
+    case BUTTONS::Y:
+    case BUTTONS::R1:
+    case BUTTONS::R2:
+      return address[0x00];
+    case BUTTONS::plus:
+    case BUTTONS::minus:
+    case BUTTONS::home:
+    case BUTTONS::share:
+    case BUTTONS::L3:
+    case BUTTONS::R3:
+      return address[0x01];
+    case BUTTONS::L1:
+    case BUTTONS::L2:
+      return address[0x02];
     default:
-      throw std::domain_error("ERROR: Tried to find adress of unknown button!");
+      throw std::domain_error("ERROR: Tried to find address of unknown button!");
     }
+  }
+  size_t buttons_data_address(BUTTONS button) const {
+    return buttons_data_address(button, type);
   }
 
 
-  static size_t axis_data_address_high(AXIS axis) {
+  static size_t axis_data_address_high(AXIS axis, PacketType packet) {
+    std::array<size_t, 4> address;
+    switch (packet) {
+    case PacketType::standard_input_report:
+      address[0x00] = 0x07;
+      address[0x01] = 0x08;
+      address[0x02] = 0x0A;
+      address[0x03] = 0x0B;
+      break;
+    case PacketType::packet_req:
+      address[0x00] = 0x11;
+      address[0x01] = 0x12;
+      address[0x02] = 0x14;
+      address[0x03] = 0x15;
+      break;
+    default:
+      throw std::domain_error("Error: This packet type does not contain axis data.");
+    }
     switch (axis) {
     case AXIS::axis_lx:
-      return 0x11;
+      return address[0x00];
     case AXIS::axis_ly:
-      return 0x12;
+      return address[0x01];
     case AXIS::axis_rx:
-      return 0x14;
+      return address[0x02];
     case AXIS::axis_ry:
-      return 0x15;
+      return address[0x03];
     default:
-      throw std::domain_error("ERROR: Tried to find address of unknown axis!");
+      throw std::domain_error("ERROR: Tried to find address of unknown axis.");
     }
   }
-  static size_t axis_data_address_low(AXIS axis) {
+  static size_t axis_data_address_low(AXIS axis, PacketType packet) {
+    std::array<size_t, 4> address;
+    switch (packet) {
+    case PacketType::standard_input_report:
+      address[0x00] = 0x06;
+      address[0x01] = 0x07;
+      address[0x02] = 0x09;
+      address[0x03] = 0x0A;
+      break;
+    case PacketType::packet_req:
+      address[0x00] = 0x10;
+      address[0x01] = 0x11;
+      address[0x02] = 0x13;
+      address[0x03] = 0x14;
+      break;
+    default:
+      throw std::domain_error("Error: This packet type does not contain axis data.");
+    }
     switch (axis) {
     case AXIS::axis_lx:
-      return 0x10;
+      return address[0x00];
     case AXIS::axis_ly:
-      return 0x11;
+      return address[0x01];
     case AXIS::axis_rx:
-      return 0x13;
+      return address[0x02];
     case AXIS::axis_ry:
-      return 0x14;
+      return address[0x03];
     default:
-      throw std::domain_error("ERROR: Tried to find address of unknown axis!");
+      throw std::domain_error("ERROR: Tried to find address of unknown axis.");
     }
+  }
+  size_t axis_data_address_high(AXIS axis) const {
+    return axis_data_address_high(axis, type);
+  }
+  size_t axis_data_address_low(AXIS axis) const {
+    return axis_data_address_low(axis, type);
   }
 
 
@@ -276,22 +332,34 @@ public:
     }
   }
 
-  static size_t dpad_data_address(DPAD dpad) {
-    switch (dpad) {
-    case d_left:
-    case d_right:
-    case d_up:
-    case d_down:
-      return 0x0f;
-    case d_none:
-      return 0x00;
+  static size_t dpad_data_address(DPAD dpad, PacketType packet) {
+    std::array<size_t, 1> address;
+    switch (packet) {
+    case PacketType::standard_input_report:
+      address[0x00] = 0x05;
+      break;
+    case PacketType::packet_req:
+      address[0x00] = 0x0f;
+      break;
     default:
-      throw std::domain_error("ERROR: Tried to find adress of unknown dpad button!");
+      throw std::domain_error("Error: This packet type does not contain dpad data.");
     }
+    switch (dpad) {
+    case DPAD::d_left:
+    case DPAD::d_right:
+    case DPAD::d_up:
+    case DPAD::d_down:
+      return address[0x00];
+    default:
+      throw std::domain_error("ERROR: Tried to find address of unknown dpad button.");
+    }
+  }
+  size_t dpad_data_address(DPAD dpad) const {
+    return dpad_data_address(dpad, type);
   }
 
 
-  static void print_exchange_array(HidApi::default_packet arr) {
+  static void print_exchange_array(HidApi::default_packet arr, PacketType type) {
     bool redcol = false;
     if (arr[0] != 0x30)
       PrintColor::yellow();
@@ -299,7 +367,8 @@ public:
       PrintColor::red();
       redcol = true;
     }
-    for (size_t i = 0; i < 0x32; ++i) {
+    size_t len = (type == PacketType::standard_input_report ? 0x32 : 0x20);
+    for (size_t i = 0; i < len; ++i) {
       if (arr[i] == 0x00) {
         PrintColor::blue();
       } else {
