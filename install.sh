b115f6f7beb63fac594b970a1f25e5d0a6992fae
angie <angheloalf95@gmail.com>
2020-10-09 17:31:59 -0300

    update udev rules

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Build SwitchProCon
-./build.sh
-
-#copy udev rules
-sudo cp 99-hid-procon.rules /etc/udev/rules.d/99-hid-procon.rules
-sudo cp 50-uinput.rules /etc/udev/rules.d/50-uinput.rules
---
+++ install_rules.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "This script installs rules for supported controllers. Without those rules, the driver would always need root access to communicate with the controllers."
+echo "This install script is optional, but highly recommended."
+
+if [[ $EUID > 0 ]]; then
+  echo ""
+  echo "Non-root user detected. If you have issues running this script, try running as root."
+fi
+
+echo ""
+
+#copy udev rules
+echo "Installing rule: 71-nintendo-controllers.rules"
+cp udev_rules/71-nintendo-controllers.rules /etc/udev/rules.d/71-nintendo-controllers.rules
+
+
+retVal=$?
+echo ""
+if [ $retVal -ne 0 ]; then
+  echo "Error."
+  echo "You could try again as root (sudo ./install_rules.sh)"
+else
+  echo "Successful."
+  echo "Please restart the device to apply the changes."
+fi
+exit $retVal
