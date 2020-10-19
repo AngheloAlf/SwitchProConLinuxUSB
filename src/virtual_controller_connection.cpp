9094f7a2ca76c1fdebc03dfd8c68743049d2153e
angie <angheloalf95@gmail.com>
2020-10-18 21:34:59 -0300

    virtualconnection: add rumble

--- src/virtual_controller_connection.cpp
+++ src/virtual_controller_connection.cpp
@@ -82,7 +82,7 @@ void ControllerConnection::setupAxis(const std::vector<uint16_t> &ids, const std
   }
 }
 
-void ControllerConnection::setupForceFeedback(const std::vector<uint16_t> &ids, uint32_t max_effects) {
+void ControllerConnection::setupForceFeedback(const std::vector<uint16_t> &ids, uint16_t max_effects) {
   if (created) {
     throw std::runtime_error("ControllerConnection.setupForceFeedback(): Already created.");
   }
@@ -97,6 +97,7 @@ void ControllerConnection::setupForceFeedback(const std::vector<uint16_t> &ids, 
   }
 
   dev.ff_effects_max = max_effects;
+  effects_amount = max_effects;
 }
 
 void ControllerConnection::createController() {
@@ -135,11 +136,27 @@ void ControllerConnection::event_sendReport() {
   sendEvent(EV_SYN, SYN_REPORT, 0);
 }
 
-/*
-void ControllerConnection::event_recvState() {
 
+void ControllerConnection::event_recvState(std::vector<Rumble> &effects) {
+  struct input_event event;
+  ssize_t ret = recvEvent(event);
+  while (ret > 0) {
+    switch (event.type) {
+    case EV_UINPUT:
+      handle_EV_UINPUT(event, effects);
+      break;
+
+    case EV_FF:
+      handle_EV_FF(event, effects);
+      break;
+
+    default:
+      printf("Unkonwn type: %x %x %i\n", event.type, event.code, event.value);
+      break;
+    }
+    ret = recvEvent(event);
+  }
 }
-*/
 
 
 
@@ -155,6 +172,88 @@ void ControllerConnection::sendEvent(uint16_t type, uint16_t code, int32_t value
   int ret = write(fd, &event, sizeof(event));
   if (ret < 0) {
     throw std::system_error(errno, std::generic_category(), "VirtualController: write error.");
+  }
+}
+
+ssize_t ControllerConnection::recvEvent(struct input_event &event) {
+  memset(&event, 0, sizeof(event));
+
+  ssize_t ret = read(fd, &event, sizeof(event));
+  /*if (ret == 0) {
+    throw std::runtime_error("ERROR: read on virtual controller returned"
+                                + std::to_string(ret) + "\n"
+                                "Maybe try running with sudo\n"
+                                + strerror(errno) + "\n");
+  }*/
+  return ret;
+}
+
+void ControllerConnection::handle_EV_UINPUT(const struct input_event &event, std::vector<Rumble> &effects) {
+  struct uinput_ff_upload upload;
+  struct uinput_ff_erase erase;
+  switch (event.code) {
+  case UI_FF_UPLOAD:
+    memset(&upload, 0, sizeof(struct uinput_ff_upload));
+    upload.request_id = event.value;
+
+    ioctl(fd, UI_BEGIN_FF_UPLOAD, &upload);
+
+    //printf("(UI_FF_UPLOAD): %i\n", event.value);
+    //printf("%x %x\n", upload.request_id, upload.retval);
+    //printf("\t%x %x %x\n", upload.effect.type, upload.effect.id, upload.effect.direction);
+    //printf("\ttrigger: %x %x\n", upload.effect.trigger.button, upload.effect.trigger.interval);
+    //printf("\treplay: %i %i\n", upload.effect.replay.length, upload.effect.replay.delay);
+    if (upload.effect.type == FF_RUMBLE) {
+      //printf("\tmagnitude: %x %x\n", upload.effect.u.rumble.strong_magnitude, upload.effect.u.rumble.weak_magnitude);
+    }
+    //printf("\n");
+    upload.retval = 0; // -1 on error
+    //upload.effect.id = 0;
+
+    if (upload.effect.id < effects_amount) {
+      struct ff_effect *eff = &upload.effect;
+      if (!effects.at(eff->id).init(eff->id, eff->type, eff->replay.length, eff->replay.delay, eff->u.rumble.strong_magnitude, eff->u.rumble.weak_magnitude)) {
+        upload.retval = -1;
+      }
+    }
+    else {
+      upload.retval = -1;
+    }
+
+    ioctl(fd, UI_END_FF_UPLOAD, &upload);
+    break;
+
+  case UI_FF_ERASE:
+    memset(&erase, 0, sizeof(struct uinput_ff_erase));
+    erase.request_id = event.value;
+
+    ioctl(fd, UI_BEGIN_FF_ERASE, &erase);
+
+    //printf("(UI_FF_ERASE): %i\n", event.value);
+    //printf("%x %x %x\n", erase.request_id, erase.retval, erase.effect_id);
+    //printf("\n");
+
+    if (erase.effect_id < effects_amount) {
+      effects.at(erase.effect_id).deinit();
+      erase.retval = 0;
+    }
+    else {
+      erase.retval = -1;
+    }
+
+    ioctl(fd, UI_END_FF_ERASE, &erase);
+    break;
+
+  default:
+    printf("(EV_UINPUT) Unkonwn code: %x %i\n", event.code, event.value);
+    break;
+  }
+}
+
+void ControllerConnection::handle_EV_FF(const struct input_event &event, std::vector<Rumble> &effects) {
+  //printf("(EV_FF) code: %04x - value: %x\n", event.code, event.value);
+  if (event.code < effects_amount) {
+    effects.at(event.code).startEffect(event.code, event.value);
   }
 }
 
--- src/virtual_controller_connection.hpp
+++ src/virtual_controller_connection.hpp
@@ -7,6 +7,8 @@
 #include <cstdint>
 #include <vector>
 
+#include "virtual_controller_rumble.hpp"
+
 namespace VirtualController {
   class ControllerConnection {
   public:
@@ -23,22 +25,27 @@ namespace VirtualController {
 
     void setupButtons(const std::vector<uint16_t> &ids);
     void setupAxis(const std::vector<uint16_t> &ids, const std::vector<int32_t> &mins, const std::vector<int32_t> &maxs);
-    void setupForceFeedback(const std::vector<uint16_t> &ids, uint32_t max_effects);
+    void setupForceFeedback(const std::vector<uint16_t> &ids, uint16_t max_effects);
     void createController();
-
 
     void event_axis(uint16_t which, int32_t value);
     void event_pressButton(uint16_t which);
     void event_releaseButton(uint16_t which);
     void event_sendReport();
 
-    //void event_recvState();
+    void event_recvState(std::vector<Rumble> &effects);
 
   private:
     void sendEvent(uint16_t type, uint16_t code, int32_t value);
+    ssize_t recvEvent(struct input_event &event);
+
+    void handle_EV_UINPUT(const struct input_event &uinput_event, std::vector<Rumble> &effects);
+    void handle_EV_FF(const struct input_event &uinput_event, std::vector<Rumble> &effects);
 
     struct uinput_setup dev;
     int fd;
+    uint16_t effects_amount = 0;
+
     bool closed = true;
     bool created = false;
   };
